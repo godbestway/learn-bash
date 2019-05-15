552aa29a1cf1ee7d62c62bfaf6b0de7abd930d30
Chenglin Ding <godbestway@163.com>
2019-05-15 10:47:58 +0200

    add some explaination

--- 23.sh
+++ 23.sh
@@ -4,6 +4,7 @@ file=$logdir/`date +%d%H`.log
 t=`date +"%F %H:%M"`
 
 [ -d $logdir ] || mkdir -p $logdir
+#every seconds get a number, get five times
 sar -n DEV 1 5| grep ens33|grep "Average" > /tmp/sar.tmp
 echo "$t" >> $file
 awk '{print "ens33:""input:",$5*8000"bps""\n""output:",$6*8000"bps"}' /tmp/sar.tmp  >>$file
